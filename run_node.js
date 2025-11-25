dd06c9f778ed938b51ab971af68834b3c6a1b192
Atul Kumawat <atulkumawat@insessor.com>
2025-11-25 11:58:48 +0530

    test

---
+++ run_node.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var app = express();
+var http = require('http'),
+    fs = require('fs'),
+    ccav = require('./ccavutil.js'),
+    qs = require('querystring'),
+    ccavReqHandler = require('./ccavRequestHandler.js'),
+    ccavResHandler = require('./ccavResponseHandler.js');
+
+app.use(express.static('public'));
+app.set('views', __dirname + '/public');
+app.engine('html', require('ejs').renderFile);
+
+
+app.get('/', function (req, res){
+    	res.render('dataFrom.html');
+});
+
+app.get('/about', function (req, res){
+    	res.render('dataFrom.html');
+});
+
+app.post('/ccavRequestHandler', function (request, response){
+	ccavReqHandler.postReq(request, response);
+});
+
+
+app.post('/ccavResponseHandler', function (request, response){
+        ccavResHandler.postRes(request, response);
+});
+
+app.listen(3002, function() {
+    console.log('Server running on http://localhost:3002');
+});
